c0376f4eed67e17fd0fcb99ae4058795efa87299
jshafto <jpshafto@gmail.com>
2020-07-20 19:05:56 -0400

    server responds to all specified requests

--- server.js
+++ server.js
@@ -2,9 +2,41 @@ const http = require("http");
 
 http.createServer(function(req, res){
     if(req.url === "/OK"){
-        console.log("Inbound 'OK' reqeust being processed");
+        console.log("Inbound 'OK' request being processed");
         res.writeHead(200)
         res.end()
+    } else if (req.url==="/Bad-Request") {
+        console.log("Bad Request");
+        res.writeHead(400);
+        res.end();
+    } else if (req.url === "/Created") {
+        console.log("created");
+        res.writeHead(201)
+        res.end()
+    } else if (req.url === "/Forbidden") {
+        console.log("Forbidden");
+        res.writeHead(403);
+        res.end()
+    } else if (req.url === "/Found") {
+        console.log("Found");
+        res.writeHead(302);
+        res.end()
+    } else if (req.url === "/Gateway-Timeout") {
+        console.log("Gateway-Timeout");
+        res.writeHead(504);
+        res.end()
+    } else if (req.url === "/Internal-Server-Error") {
+        console.log("Internal-Server-Error");
+        res.writeHead(500);
+        res.end()
+    } else if (req.url === "/Moved-Permanently") {
+        console.log("Moved-permanently");
+        res.writeHead(301);
+        res.end()
+    } else if (req.url === "/Unauthorized") {
+        console.log("Unauthorized");
+        res.writeHead(401);
+        res.end();
     } else {
         res.writeHead(404)
         res.end()
@@ -13,3 +45,9 @@ http.createServer(function(req, res){
 }).listen(3000, function(){
     console.log("Listening on port 3000")
 })
+// forbidden 403
+// found 302
+// gateway-timeout 504
+// internal server error 500
+// moved-permanently 301
+// unauthorized
