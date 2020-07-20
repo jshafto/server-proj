51f87ad5054044a20f20a54966a26800cee39ce7
jshafto <jpshafto@gmail.com>
2020-07-20 18:54:50 -0400

    added /200 page and 404 if anything else is requested

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 http.createServer(function(req, res){
-    res.end()
+    if(req.url === "/OK"){
+        console.log("Inbound 'OK' reqeust being processed");
+        res.writeHead(200)
+        res.end()
+    } else {
+        res.writeHead(404)
+        res.end()
+    }
+
 }).listen(3000, function(){
     console.log("Listening on port 3000")
 })
